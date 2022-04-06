c3184451ca771d9f04e612332d29e8d825c9dd6e
contability <shouldBnewB@gmail.com>
2022-04-07 00:45:23 +0900

    study중

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+//이렇게 함수로 만들어진 컴포넌트를 함수형 컴포넌트라고 함.
+//모든 컴포넌트는 대문자로 시작.
+//이 함수가 return하는 것은 JSX이다.(JavaScript XML)
 function App() {
+  const name = "Tom";
+  const naver = {
+    name : '네이버',
+    url : 'https://naver.com'
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          !!!!
-        </a>
-      </header>
+      <h1 style={
+        {
+          color: '#f0f',
+          backgroundColor: "green"
+        }}>Hello, {name}.
+        <p>{2+3}</p>
+        </h1>
+        <a href={naver.url}>{naver.name}</a>
     </div>
   );
 }
 
+//이걸 이제 index.html에서 받아다가 root에 DOM 구현
 export default App;
