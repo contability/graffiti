4648509a2859e72fd407fbca871a7df56bb64cb6
contability <shouldBnewB@gmail.com>
2022-05-13 16:27:11 +0900

    에러 수정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -27,17 +27,16 @@ function App() {
       <button onClick={searchButtonTapped}>포켓몬 찾기</button>
       {/* pokemonReducer가 success일 때만 div태그 띄움 */}
       {pokemonReducer.success && <div> 
-          <p>{pokemonName}</p>
+          <p>pokemonName : {pokemonName}</p>
           {/* pokemon? 이것처럼 Question 마크가 있다면 이 객체가 있을 때만 보여주겠다는 뜻 */}
           {pokemonReducer.pokemon?.abilities.map((ability) => {
-            return 
-              <div>
-                <p>{ability.ability.name}</p>
-                <p>{ability.slot}</p>
-
-              </div>
+            return (
+              <div key={ability.ability.name}>
+                <p>abilityName : {ability.ability.name}</p>
+                <p>abilitySlot : {ability.slot}</p>
+              </div>)
           })}
-          <img src={pokemonReducer.pokemon?.sprites.front_default} alt={pokemonReducer.pokemon?.sprites.front_default} />
+          <img src={pokemonReducer.pokemon?.sprites.front_default} alt={pokemonReducer.pokemon?.sprites.front_default} title={pokemonName} />
           </div>}
     </div>
   );
