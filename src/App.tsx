db54df0b000a5751b11b49ab9213fa4a84820996
FURIUM <shouldBnewB@gmail.com>
2022-08-19 20:12:07 +0900

    ing build

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Main from './routers/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <AppBox>
+      <Main/>
+    </AppBox>
   );
 }
+
+const AppBox = styled.main``;
 
 export default App;
---
+++ src/routers/Main.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Main : Function = () => {
+
+    return (
+        <MainBox>
+            wefwfwf
+        </MainBox>
+    );
+};
+
+const MainBox = styled.div``;
+
+export default Main;
