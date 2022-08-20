39331dc2ff8bc59af79876da4dfab32e2822360d
contability <shouldBnewB@gmail.com>
2022-08-20 12:08:15 +0900

    ing header

---
+++ src/components/header/DefaultHeader.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const DefaultHeader: Function = () => {
+
+};
+
+const BREAK_POINT_MOBILE = 360;
+const BREAK_POINT_TABLET = 1024;
+const BREAK_POINT_PC = 1920;
+
+const DefaultHeaderBox = styled.header`
+
+`;
+
+export default DefaultHeader;
