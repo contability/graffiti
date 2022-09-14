494838d4f98cbdb0d0f7b22f68704b514ce77510
contability <shouldBnewB@gmail.com>
2022-09-15 01:49:15 +0900

    (router) add ReactHookForm

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './components/GlobalStyle';
 import Main from './routers/Main';
+import ReactHookForm from './routers/ReactHookForm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <GlobalStyle/>  {/* 전체 css 잡아주기*/}
         <Routes>    {/* 이 안에 router들 정의하기 */}
           <Route path='/' element={<Main/>}/>   {/* path마다 어떤 컴포넌트 띄워줄건지 정의 */}
+          <Route path='/ReactHookForm' element={<ReactHookForm/>}/>
         </Routes>
       </HashRouter>
     </AppBox>
