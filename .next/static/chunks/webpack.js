ffb49e53e38dab7d3b07690e9086835c356e49c4
contability <shouldBnewB@gmail.com>
2022-12-15 23:47:34 +0900

    (services) add insert todo color brush

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ffce29750480a1be"; }
+/******/ 		__webpack_require__.h = function() { return "1f87c47f7130e248"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
