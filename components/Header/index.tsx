93ac1b6f6dd31184dedb8bdc9e49cee74c137d0c
FURIUM <shouldBnewB@gmail.com>
2023-03-03 17:55:47 +0900

    (services) add user menu

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -10,6 +10,8 @@ import { useSelector } from '../../store';
 import HamburgerIcon from '../../public/assets/images/icons/system/system_hamburger.svg';
 import AuthModal from '../auth/AuthModal';
 import AVD from '../../public/assets/images/profile/profile_user01.svg';
+import OutsideClickHandler from 'react-outside-click-handler';
+import { useRouter } from 'next/router';
 
 const Container = styled.div`
   position: sticky;
@@ -30,6 +32,40 @@ const Container = styled.div`
 
     .header-logo {
       margin-right: 6px;
+    }
+  }
+
+  .header-logo-wrapper + div {
+    position: relative;
+  }
+
+  .header-usermenu {
+    position: absolute;
+    right: 0;
+    top: 52px;
+    width: 240px;
+    padding: 8px 0;
+    box-shadow: 0 2px 16px rgba(0, 0, 0, 0.12);
+    border-radius: 8px;
+    background-color: white;
+
+    li {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 42px;
+      padding: 0 16px;
+      cursor: pointer;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+
+    .header-usermenu-divider {
+      width: 100%;
+      height: 1px;
+      margin: 8px 0;
+      background-color: ${palette.gray_dd};
     }
   }
 
@@ -115,8 +151,11 @@ const Container = styled.div`
 
 const Header: React.FC = () => {
   // const [modalOpened, setModalOpened] = useState(false);
+  const [isUsermenuOpened, setIsUsermenuOpened] = useState(false);
   const { openModal, closeModal, ModalPortal } = useModal();
   const user = useSelector(state => state.user);
+
+  const router = useRouter();
 
   return (
     <Container>
@@ -131,10 +170,18 @@ const Header: React.FC = () => {
           <button className="header-login-button">로그인</button>
         </div>
       ) : (
-        <button className="header-user-profile" type="button">
-          <HamburgerIcon />
-          <img src={user.profileImage} alt="" className="header-user-profile-image" />
-        </button>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            if (isUsermenuOpened) setIsUsermenuOpened(false);
+          }}
+        >
+          <button className="header-user-profile" type="button" onClick={() => setIsUsermenuOpened(!isUsermenuOpened)}>
+            <HamburgerIcon />
+            {/* <img src={user.profileImage} alt="" className="header-user-profile-image" /> */}
+            <AVD className="header-user-profile-image" />
+          </button>
+          {/* {isUsermenuOpened && <div>유저 메뉴</div>} */}
+        </OutsideClickHandler>
       )}
       {/* {modalOpened && (
         // modal
@@ -154,6 +201,30 @@ const Header: React.FC = () => {
           <AuthModal closeModal={closeModal} />
         </ModalPortal>
       }
+      {isUsermenuOpened && (
+        <ul className="header-usermenu">
+          <li>숙소 관리</li>
+          {/* Link는 a 태그로 랜더링 되므로 유효하지 않다. 옛날 버전에선 됐던 것 같다. (legacyBehavior) */}
+          {/* <Link href="/room/register/building" legacyBehavior>
+            <a role="presentation" onClick={() => setIsUsermenuOpened(false)}>
+              <li>숙소 등록하기</li>
+            </a>
+          </Link> */}
+          <Link
+            href=""
+            onClick={() => {
+              setIsUsermenuOpened(false);
+              router.push('/room/register/building');
+            }}
+          >
+            <li>숙소 등록하기</li>
+          </Link>
+          <div className="header-usermenu-divider" />
+          <li role="presentation" onClick={() => {}}>
+            로그아웃
+          </li>
+        </ul>
+      )}
     </Container>
   );
 };
