6499a7a0898e57bed430279ac9703fb2dc6c433a
contability <shouldBnewB@gmail.com>
2023-03-29 00:20:44 +0900

    (services) add register room footer

--- components/common/Button/index.tsx
+++ components/common/Button/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import palette from '../../../styles/palette';
 
 const Container = styled.button`
@@ -13,14 +13,33 @@ const Container = styled.button`
   font-weight: 800;
   outline: none;
   cursor: pointer;
+  ${props => getButtonColor(props.color || '')}
 `;
+
+const getButtonColor = (color: string) => {
+  switch (color) {
+    case 'dark_cyan':
+      return css`
+        background-color: ${palette.dark_cyan};
+      `;
+    default:
+      return css`
+        background-color: ${palette.bittersweet};
+      `;
+  }
+};
 
 interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  color?: 'dark_cyan';
 }
 
-const Button: React.FC<IProps> = ({ children, ...props }) => {
-  return <Container {...props}>{children}</Container>;
+const Button: React.FC<IProps> = ({ children, color, ...props }) => {
+  return (
+    <Container {...props} color={color}>
+      {children}
+    </Container>
+  );
 };
 
 // props의 값이 같다면 리렌더링 방지. 최적화
