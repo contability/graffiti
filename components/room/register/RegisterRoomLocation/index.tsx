74169d3770bd3c1d8dd04350862f3440efa0f3ea
contability <shouldBnewB@gmail.com>
2023-04-24 18:47:38 +0900

    (services) add pick coordinates

--- components/room/register/RegisterRoomLocation/index.tsx
+++ components/room/register/RegisterRoomLocation/index.tsx
@@ -37,6 +37,11 @@ const Container = styled.div`
   }
 `;
 
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
 const RegisterRoomLocation: React.FC = () => {
   const country = useSelector(state => state.registerRoom.country);
   const city = useSelector(state => state.registerRoom.city);
@@ -77,13 +82,28 @@ const RegisterRoomLocation: React.FC = () => {
     dispatch(registerRoomActions.setPostcode(event.target.value));
   };
 
+  /** 현재 위치 불러오기 성공했을 때 */
+  const onSuccessGetLocation = ({ coords }: { coords: Coordinates }) => {
+    console.log('latitude', coords.latitude);
+    console.log('longitude', coords.longitude);
+  };
+
+  /** 현재 위치 사용 클릭 시 */
+  const onClickGetCurrentLocation = () => {
+    // navigator.geolocation.getCurrentPosition(suceesFunction, failFunction);
+    navigator.geolocation.getCurrentPosition(onSuccessGetLocation, e => {
+      console.log(e);
+      alert(e?.message);
+    });
+  };
+
   return (
     <Container>
       <h2>숙소의 위치를 알려주세요.</h2>
       <h3>4단계</h3>
       <p className="register-room-step-info">정확한 숙소 주소는 게스트가 예약을 완료한 후에만 공개됩니다.</p>
       <div className="register-room-location-button-wrapper">
-        <Button color="dark_cyan" colorReverse icon={<NavigationIcon />}>
+        <Button color="dark_cyan" colorReverse icon={<NavigationIcon />} onClick={onClickGetCurrentLocation}>
           현재 위치 사용
         </Button>
       </div>
