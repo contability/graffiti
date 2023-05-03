8982bbb2d194eebacfe609920077d0b82b767235
contability <shouldBnewB@gmail.com>
2023-05-03 19:04:55 +0900

    (services) add s3 upload

--- pages/api/files/upload.ts
+++ pages/api/files/upload.ts
@@ -1,5 +1,7 @@
 import formidable from 'formidable';
 import { NextApiRequest, NextApiResponse } from 'next';
+import aws from 'aws-sdk';
+import { createReadStream } from 'fs';
 
 export const config = {
   api: {
@@ -11,9 +13,34 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     try {
       const form = new formidable.IncomingForm();
-      form.parse(req, async (err, fields, files) => {
-        console.log(files);
+
+      const url = await new Promise((resolve, reject) => {
+        form.parse(req, async (err, fields, files) => {
+          const s3 = new aws.S3({
+            accessKeyId: process.env.ACCESSKEY_ID,
+            secretAccessKey: process.env.SECRET_ACCESSKEY_ID,
+          });
+
+          const stream = createReadStream(files.file.filepath);
+
+          await s3
+            .upload({
+              Bucket: process.env.S3_BUCKET_NAME!,
+              Key: files.file.originalFilename,
+              ACL: 'public-read',
+              Body: stream,
+            })
+            .promise()
+            .then(res => {
+              console.log(res);
+              resolve(res.Location);
+            })
+            .catch(e => reject(e));
+        });
       });
+
+      res.statusCode = 201;
+      res.send(url);
     } catch (e) {
       console.error(e);
       res.end();
@@ -21,6 +48,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   res.statusCode = 405;
-
   return res.end();
 };
