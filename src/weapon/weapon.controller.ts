c28baec9678f0b638191480fef58cd402bfd5c44
contability <shouldBnewB@gmail.com>
2023-05-18 00:41:13 +0900

    (weapon) add controller

--- src/weapon/weapon.controller.ts
+++ src/weapon/weapon.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Redirect, Req } from '@nestjs/common'
+import { WeaponDTO } from './weapon.dto'
 
 @Controller('weapon')
 export class WeaponController {
-  @Post()
-  create(): string{
-        return 'new weapon';
-    }
+	@Get('list')
+	findAll(): string {
+		return 'weapon list'
+	}
 
-    @Get()
-    findAll(): string {
-        return 'weapon list';
-    }
+	@Get(':name')
+	findOne(@Param('name') name: string): string {
+		console.log(`${name}  is weapon`)
+		return `${name} is weapon`
+	}
+
+	@Post()
+	@Redirect('localhost:3000/weapon/list', 204)
+	create(@Body() weaponDTO: WeaponDTO) {
+		return 'new weapon'
+	}
+
+	@Patch()
+	// FIXME : pk 빼고 옵셔널하게 타입 재설정
+	update(@Body() weaponDTO: WeaponDTO) {
+		return 'update Weapon'
+	}
+
+	@Delete(':id')
+	remove(@Param('id') id: string) {
+		return 'remove weapon'
+	}
 }
---
+++ src/weapon/weapon.dto.ts
@@ -0,0 +1,5 @@
+export class WeaponDTO {
+	id: number
+	name: string
+	portability: boolean
+}
