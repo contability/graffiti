6d6bfd38e18b30a74c3c10c7c6fadc75a495006a
contability <shouldBnewB@gmail.com>
2023-05-31 01:53:37 +0900

    intermediate commit

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -1,21 +1,24 @@
 import { useEffect } from "react";
 import Header from "../common/Header";
-import { BetaAnalyticsDataClient } from "@google-analytics/data";
+// import { BetaAnalyticsDataClient } from "@google-analytics/data";
 
 const Statistics = () => {
   const propertyId = process.env.NEXT_PUBLIC_GA_ID;
   //   const CLIENT_ID = process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID;
+  //   const PRIVATE_KEY = process.env.NEXT_PUBLIC_OAUTH_CLIENT_PRIVATE_KEY;
   //   const SCOPES = ["https://www.googleapis.com/auth/analytics.readonly"];
-  const analyticsDataClient = new BetaAnalyticsDataClient();
+  // const analyticsDataClient = new BetaAnalyticsDataClient();
 
   const sampleRunReport = () => {
     // fetch(
-    //   `https://analyticsdata.googleapis.com/v1beta/properties/${propertyId}:runReport`,
+    //   // `https://analyticsdata.googleapis.com/v1beta/properties/${propertyId}:runReport`,
+    //   `https://analyticsadmin.googleapis.com/v1beta/properties/${propertyId}:acknowledgeUserDataCollection`,
     //   {
     //     method: "POST",
     //     headers: {
     //       "Content-Type": "application/json",
     //     },
+    //     body: JSON.stringify(data),
     //   }
     // )
     //   .then((res) => {
@@ -25,33 +28,46 @@ const Statistics = () => {
     //   .catch((e) => {
     //     console.error(e);
     //   });
-
-    const response = analyticsDataClient.runReport({
-      property: `properties/${propertyId}`,
-      dateRanges: [
-        {
-          startDate: "2023-05-26",
-          endDate: "today",
-        },
-      ],
-      dimensions: [
-        {
-          name: "deviceCategory",
-        },
-      ],
-      metrics: [
-        {
-          name: "activeUsers",
-        },
-      ],
-    });
-    console.log("report result:", JSON.stringify(response.row));
-
+    fetch(
+      `https://www.googleapis.com/analytics/v3/data/ga?ids=ga:${propertyId}&start-date=2023-05-26&end-date=2023-05-30&metrics=ga:session,ga:bounces`,
+      {
+        method: "GET",
+        // headers: {
+        //   "Content-Type": "application/json",
+        // },
+        // body: JSON.stringify(data),
+      }
+    )
+      .then((res) => {
+        console.log(res);
+        // if (res) return res;
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+    // const response: any = analyticsDataClient.runReport({
+    //   property: `properties/${propertyId}`,
+    //   dateRanges: [
+    //     {
+    //       startDate: "2023-05-26",
+    //       endDate: "today",
+    //     },
+    //   ],
+    //   dimensions: [
+    //     {
+    //       name: "deviceCategory",
+    //     },
+    //   ],
+    //   metrics: [
+    //     {
+    //       name: "activeUsers",
+    //     },
+    //   ],
+    // });
+    // console.log("report result:", JSON.stringify(response.row));
     // const scope = "https://www.googleapis.com/auth/analytics.readonly";
     // const keyFileLocation = "../../../arboreal-retina-388303-87db528f8f27.json";
-
     // const VIEW_ID = "378781622";
-
     // gapi.client
     //   .request({
     //     path: "https://analyticsreporting.googleapis.com/v4/reports:batchGet",
--- src/pages/api/auth/callback/google.ts
+++ src/pages/api/auth/callback/google.ts
@@ -1,5 +1,17 @@
-export default (req: any, res: any) => {
-  console.log("here", req);
+import { google } from "googleapis";
+import { NextApiRequest, NextApiResponse } from "next";
 
-  console.log("here", res);
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const propertyId = process.env.NEXT_PUBLIC_GA_ID;
+  const CLIENT_ID = process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID;
+  const PRIVATE_KEY = process.env.NEXT_PUBLIC_OAUTH_CLIENT_PRIVATE_KEY;
+
+  const startDate = req.query.startDate || "2023-05-26";
+  const post = req.query.post;
+
+  try {
+    window.gapi.client.request({
+      path: "/v4/reports:batchGet",
+    });
+  } catch (e) {}
 };
