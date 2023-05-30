a4c06118dfbded3842f0281ce77d8144ea5534a0
furium <shouldBnewB@gmail.com>
2023-05-30 16:32:31 +0900

    add test getData

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -1,27 +1,51 @@
 import { useEffect } from "react";
 import Header from "../common/Header";
+import { BetaAnalyticsDataClient } from "@google-analytics/data";
 
 const Statistics = () => {
   const propertyId = process.env.NEXT_PUBLIC_GA_ID;
+  const analyticsDataClient = new BetaAnalyticsDataClient();
 
   const sampleRunReport = () => {
-    fetch(
-      `https://analyticsdata.googleapis.com/v1beta/properties/${propertyId}:runReport`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((res) => {
-        console.log(res);
+    // fetch(
+    //   `https://analyticsdata.googleapis.com/v1beta/properties/${propertyId}:runReport`,
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // )
+    //   .then((res) => {
+    //     console.log(res);
 
-        // if (res) return res;
-      })
-      .catch((e) => {
-        console.error(e);
-      });
+    //     // if (res) return res;
+    //   })
+    //   .catch((e) => {
+    //     console.error(e);
+    //   });
+
+    const response = analyticsDataClient.runReport({
+      property: `properties/${propertyId}`,
+      dateRanges: [
+        {
+          startDate: "2023-05-26",
+          endDate: "today",
+        },
+      ],
+      dimensions: [
+        {
+          name: "city",
+        },
+      ],
+      metrics: [
+        {
+          name: "activeUsers",
+        },
+      ],
+    });
+
+    console.log("report result:", response);
   };
   useEffect(() => {
     sampleRunReport();
