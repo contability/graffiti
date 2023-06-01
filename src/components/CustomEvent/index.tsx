0026df35f10122909c156138bb53f2cb08a751ee
furium <shouldBnewB@gmail.com>
2023-06-01 17:01:23 +0900

    add analytics property

---
+++ src/components/CustomEvent/index.tsx
@@ -0,0 +1,24 @@
+import * as gtag from '../../../lib/gtag';
+
+const CustomEvent: React.FC = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    // 이런 식으로 이벤트 보내기 가능
+    gtag.event({
+      action: 'custom_event',
+      category: 'ce',
+      label: '커스텀 이벤트',
+      value: 0,
+    });
+  };
+
+  return (
+    <div>
+      <h1>커스텀 이벤트</h1>
+      <button onClick={handleSubmit}>submit</button>
+    </div>
+  );
+};
+
+export default CustomEvent;
