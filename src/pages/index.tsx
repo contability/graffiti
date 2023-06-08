d5b62644e02804d0b1614349b2623797f65f1cc4
furium <shouldBnewB@gmail.com>
2023-06-08 13:45:14 +0900

    commit test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NextPage } from "next";
 
 const index: NextPage = () => {
-  return <div>hello nextJs</div>;
+  return <div>hello nextJswef</div>;
 };
 
 export default index;
