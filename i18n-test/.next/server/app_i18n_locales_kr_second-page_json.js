80dd589f74eafdf4565a5a20c55cfe8663d91427
contability <shouldBnewB@gmail.com>
2023-06-09 02:08:41 +0900

    add i18n-test

---
+++ i18n-test/.next/server/app_i18n_locales_kr_second-page_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "app_i18n_locales_kr_second-page_json";
+exports.ids = ["app_i18n_locales_kr_second-page_json"];
+exports.modules = {
+
+/***/ "./app/i18n/locales/kr/second-page.json":
+/*!**********************************************!*\
+  !*** ./app/i18n/locales/kr/second-page.json ***!
+  \**********************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"title":"두 번째 페이지임","back-to-home":"메인으로"}');
+
+/***/ })
+
+};
+;
---
+++ i18n-test/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,7 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {},
+  "cssModules": {
+    "D:\\project\\graffiti\\i18n-test\\app\\[lng]\\page": [],
+    "D:\\project\\graffiti\\i18n-test\\app\\[lng]\\second-page\\page": []
+  }
+}
