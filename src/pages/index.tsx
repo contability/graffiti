60c2bb0f3dcfb0d265972d86ee300b651f0d1275
furium <shouldBnewB@gmail.com>
2023-06-08 15:45:32 +0900

    awfawf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NextPage } from "next";
 
 const index: NextPage = () => {
-  return <div>git hook awwa</div>;
+  return <div>git hook</div>;
 };
 
 export default index;
