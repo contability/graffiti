582a4a1b0c22c560d0aa1a8559d354c5ea04e07a
furium <shouldBnewB@gmail.com>
2023-06-08 15:44:55 +0900

    (Add) ee

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NextPage } from "next";
 
 const index: NextPage = () => {
-  return <div>git hook</div>;
+  return <div>git hook awwa</div>;
 };
 
 export default index;
