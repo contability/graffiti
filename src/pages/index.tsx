fd0d44ed06aa6cd65dc30112d1e9326472da50c5
furium <shouldBnewB@gmail.com>
2023-06-09 12:23:33 +0900

    feat: log쌓기3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NextPage } from "next";
 
 const index: NextPage = () => {
-  return <div>git hook</div>;
+  return <div>git hook commit msg</div>;
 };
 
 export default index;
