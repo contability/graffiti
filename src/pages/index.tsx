666667e724f24f9b346e98634e82ba7248913f22
furium <shouldBnewB@gmail.com>
2023-06-15 10:04:56 +0900

    feat: test [FRONTEND-1]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NextPage } from "next";
 
 const index: NextPage = () => {
-  return <div>git hook prepare commit msg</div>;
+  return <div>git hook prepare commit msg </div>;
 };
 
 export default index;
